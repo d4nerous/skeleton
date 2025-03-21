a88d7aa2549dea600bd49fa5d288f1c0a9c36a5a
N.Manca <n.manca@reactive-almaviva.it>
2025-03-21 13:11:40 +0100

    creazione collegamento tra il bottone e la seconda view

--- src/main/java/org/vaadin/example/components/RoundButtonComponent.java
+++ src/main/java/org/vaadin/example/components/RoundButtonComponent.java
@@ -3,7 +3,7 @@ package org.vaadin.example.components;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-
+import com.vaadin.flow.router.RouterLink;
 
 public class RoundButtonComponent extends VerticalLayout {
 
@@ -12,17 +12,21 @@ public class RoundButtonComponent extends VerticalLayout {
         Button roundButton = new Button();
         roundButton.setText("Clicca qui");
         roundButton.getStyle()
-                   .set("width", "150px")
-                   .set("height", "150px")
-                   .set("border-radius", "50%")
-                   .set("background-color", "#808080") // colore grigio
-                   .set("color", "white")
-                   .set("font-size", "20px")
-                   .set("border", "none")
-                   .set("cursor", "pointer");
+                .set("width", "150px")
+                .set("height", "150px")
+                .set("border-radius", "50%")
+                .set("background-color", "#808080") // colore grigio
+                .set("color", "white")
+                .set("font-size", "20px")
+                .set("border", "none")
+                .set("cursor", "pointer");
 
+        // Aggiungi il listener di click al bottone
         roundButton.addClickListener(e -> {
-            Notification.show("Hai cliccato il bottone!");
+            // Naviga alla vista "second-view"
+            getUI().ifPresent(ui -> ui.navigate("second-view"));
+            // Opzionale: mostra una notifica per il click
+            Notification.show("Hai cliccato il bottone e sei stato reindirizzato alla seconda vista!");
         });
 
         add(roundButton);
